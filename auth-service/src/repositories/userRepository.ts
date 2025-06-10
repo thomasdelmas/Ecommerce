0781d1d9a6813927c6460fb245cf49ff0041e9cc
tdelmas42 <tdelmas@student.42.fr>
2025-06-10 16:54:32 +0200

    Feat(auth): Add deleteUsers method to UserRepository

--- auth-service/src/repositories/userRepository.ts
+++ auth-service/src/repositories/userRepository.ts
@@ -1,4 +1,4 @@
-import { HydratedDocument, Types } from 'mongoose';
+import { DeleteResult, HydratedDocument, Types } from 'mongoose';
 import { IUser } from '../types/user.js';
 import { IUserModel } from '../types/db.js';
 
@@ -8,6 +8,7 @@ export type IUserRepository = {
     username: IUser['username'],
   ) => Promise<HydratedDocument<IUser> | null>;
   getUserById: (id: string) => Promise<HydratedDocument<IUser> | null>;
+	deleteUsers: (ids: string[]) => Promise<DeleteResult | null>
 };
 
 export class UserRepository implements IUserRepository {
@@ -25,5 +26,9 @@ export class UserRepository implements IUserRepository {
   getUserById = async (id: string) => {
     const user = await this.db.findOne({ _id: new Types.ObjectId(id) });
     return user;
+  };
+
+  deleteUsers = async (ids: string[]) => {
+    return await this.db.deleteMany({ _id: { $in: ids } });
   };
 }
