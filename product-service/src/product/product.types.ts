db56ca7e9dd6cb9d286eb1c589385fe9995f5454
ThomasDelmas <thomas.delmas97@gmail.com>
2025-06-16 18:15:10 +0200

    Feat(product): Add product schema, model and types

---
+++ product-service/src/product/product.types.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from 'express';
+import mongoose from 'mongoose';
+
+export interface IProduct {
+  createdAt: number;
+  name: string;
+  category: string;
+  price: number;
+  currency: string;
+  stock: number;
+}
+
+export type IProductModel = mongoose.Model<IProduct>;
+
+export type IProductRepository = {
+  createProducts: (products: IProduct[]) => Promise<IProduct[]>;
+  getProductByName: (name: IProduct['name']) => Promise<IProduct | null>;
+};
+
+type CreateProductsPayloadOmit = 'createdAt' | 'currency';
+export interface CreateProductsPayload
+  extends Omit<IProduct, CreateProductsPayloadOmit> {}
+
+export type IProductService = {
+  createProducts: (
+    inputs: CreateProductsPayload[],
+  ) => Promise<{
+    createdProducts: IProduct[] | null;
+    failed: { input: CreateProductsPayload; reason: string }[];
+  }>;
+};
+
+export type ICreateProductsReqBody = {
+  products: CreateProductsPayload[];
+};
+
+export type IProductController = {
+  createProducts: (
+    req: Request<{}, {}, ICreateProductsReqBody>,
+    res: Response,
+  ) => Promise<any>;
+};
