02278dfd2a134fef4b26d8ae693981a49658dec9
ThomasDelmas <thomas.delmas97@gmail.com>
2025-07-01 14:34:30 +0200

    fix(product): update to match for unsorted results

--- product-service/src/__tests__/integration.test.ts
+++ product-service/src/__tests__/integration.test.ts
@@ -285,26 +285,27 @@ describe('ProductService - Integration tests', () => {
       expect(getProductsWithFilterSpy).toHaveBeenCalled();
 
       expect(res.status).toBe(200);
-      expect(res.body).toMatchObject({
-        products: [
-          {
+      expect(res.body.products).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
             name: 'Purple Pant',
             category: 'Pant',
             price: 100,
             currency: 'euro',
             stock: 30,
-          },
-          {
+          }),
+          expect.objectContaining({
             name: 'Black Pant',
             category: 'Pant',
             price: 200,
             currency: 'Dollar',
             stock: 3000,
-          },
-        ],
-        count: 2,
-        message: 'Successfuly found products',
-      });
+          }),
+        ]),
+      );
+
+      expect(res.body.count).toBe(2);
+      expect(res.body.message).toBe('Successfuly found products');
     });
 
     it('should retrieve filtered products information succesfuly with cache hit (same request than the previous)', async () => {
