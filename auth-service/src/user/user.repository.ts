301c83946db345bc2f515cd3420d77ae5dc33cc5
ThomasDelmas <thomas.delmas97@gmail.com>
2025-07-04 15:46:16 +0200

    refactor(auth): update user repo method to return lean types

--- auth-service/src/user/user.repository.ts
+++ auth-service/src/user/user.repository.ts
@@ -1,26 +1,59 @@
-import { Types } from 'mongoose';
+import { HydratedDocument } from 'mongoose';
 import type { IUserModel } from '../types/db.types.js';
-import type { IUser, IUserRepository } from './user.types.js';
+import type {
+  IUserCreation,
+  IUserRepository,
+  IUserSecure,
+} from './user.types.js';
+import type { IUserSchema } from '../models/schemas/userSchema.js';
+import lodash from 'lodash';
+const { omit } = lodash;
 
 class UserRepository implements IUserRepository {
   constructor(private db: IUserModel) {}
 
-  createUsers = async (users: IUser[]) => {
-    return await this.db.create(users);
+  createUsers = async (users: IUserCreation[]) => {
+    const newDocs = await this.db.create(users);
+    return newDocs.map((doc) => this.toIUserSecure(doc));
   };
 
-  getUserByUsername = async (username: IUser['username']) => {
+  getUserByUsername = async (username: string) => {
     const user = await this.db.findOne({ username });
-    return user;
+    return user ? this.toIUser(user) : null;
+  };
+
+  getUsersById = async (ids: string[]) => {
+    const docs = await this.db.find({ _id: { $in: ids } });
+    return docs.map((doc) => this.toIUser(doc));
   };
 
   getUserById = async (id: string) => {
-    const user = await this.db.findOne({ _id: new Types.ObjectId(id) });
-    return user;
+    const res = await this.getUsersById([id]);
+    return res.length > 0 ? res[0] : null;
   };
 
   deleteUsers = async (ids: string[]) => {
     return await this.db.deleteMany({ _id: { $in: ids } });
+  };
+
+  toIUserSecure = (doc: HydratedDocument<IUserSchema>) => {
+    const newSecure = omit(doc.toObject(), [
+      'hash',
+      '__v',
+      '_id',
+    ]) as IUserSecure;
+    return {
+      ...newSecure,
+      id: doc._id.toString(),
+    };
+  };
+
+  toIUser = (doc: HydratedDocument<IUserSchema>) => {
+    const user = doc.toObject();
+    return {
+      ...user,
+      id: doc._id.toString(),
+    };
   };
 }
 
