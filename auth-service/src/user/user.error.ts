c43e209ef42d6014e8bc65c87fe2e1e3af508c7b
ThomasDelmas <thomas.delmas97@gmail.com>
2025-07-07 19:51:56 +0200

    refactor(auth): add error factory for cleaner code

---
+++ auth-service/src/user/user.error.ts
@@ -0,0 +1,46 @@
+import { AppError } from '../errors/appError';
+
+export const Errors = {
+  RegistrationFailed: () =>
+    new AppError('Failed to register user', {
+      statusCode: 500,
+      code: 'FAILED_TO_REGISTER',
+    }),
+
+  DeletionFailed: () =>
+    new AppError('Failed to delete user', {
+      statusCode: 500,
+      code: 'FAILED_TO_DELETE',
+    }),
+
+  UserAlreadyExist: () =>
+    new AppError('User already exist', {
+      statusCode: 400,
+      code: 'USER_ALREADY_REGISTERED',
+    }),
+
+  UserNotFound: () =>
+    new AppError('User not found', { statusCode: 404, code: 'USER_NOT_FOUND' }),
+
+  InvalidPassword: () =>
+    new AppError('Invalid password', {
+      statusCode: 401,
+      code: 'INVALID_PASSWORD',
+    }),
+
+  Forbidden: () =>
+    new AppError('Forbidden operation', { statusCode: 403, code: 'FORBIDDEN' }),
+
+  RolePermission: () =>
+    new AppError('Failed to retrieve permissions for role', {
+      statusCode: 500,
+      code: 'ROLE_PERMISSION_ERROR',
+    }),
+
+  UserDeletion: (failed: { id: string; reason: string }[]) =>
+    new AppError('One or more users could not be deleted', {
+      statusCode: 500,
+      code: 'USER_DELETION_FAILED',
+      meta: { failed },
+    }),
+};
